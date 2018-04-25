65973187c51ebe4f9008cc909b9465dec3f2792d
minphone <dev.minphon@gmail.com>
2018-04-25 15:30:19 +0630

    Change Log In Background Image

--- app/src/main/java/co/minphone/happyonlinecinematicket/activity/LogInActivity.java
+++ app/src/main/java/co/minphone/happyonlinecinematicket/activity/LogInActivity.java
@@ -2,19 +2,13 @@ package co.minphone.happyonlinecinematicket.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.SurfaceTexture;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Surface;
 import android.view.TextureView;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import butterknife.BindView;
 import butterknife.OnClick;
 import co.minphone.happyonlinecinematicket.R;
@@ -30,30 +24,26 @@ import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import dagger.android.AndroidInjection;
-import java.io.IOException;
 import java.util.Arrays;
-import javax.inject.Inject;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LogInActivity extends BaseActivity<LogInPresenter>
-    implements TextureView.SurfaceTextureListener, LogInView<LogInPresenter> {
+    implements LogInView<LogInPresenter> {
 
-  @BindView(R.id.trailer) TextureView trailer;
   @BindView(R.id.btn_facebook) Button btnFacebook;
 
   private MediaPlayer mediaPlayer;
   private CallbackManager callbackManager;
 
   public static void start(Context context) {
-      Intent starter = new Intent(context, LogInActivity.class);
-      context.startActivity(starter);
+    Intent starter = new Intent(context, LogInActivity.class);
+    context.startActivity(starter);
   }
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     AndroidInjection.inject(this);
     super.onCreate(savedInstanceState);
-    trailer.setSurfaceTextureListener(this);
     registerFacebook();
   }
 
@@ -87,40 +77,6 @@ public class LogInActivity extends BaseActivity<LogInPresenter>
     callbackManager.onActivityResult(requestCode, resultCode, data);
   }
 
-  @Override public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-    Surface s = new Surface(surface);
-
-    try {
-      mediaPlayer = new MediaPlayer();
-      mediaPlayer.setDataSource(getApplicationContext(),
-          Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.trailer));
-      mediaPlayer.setSurface(s);
-      mediaPlayer.prepare();
-      mediaPlayer.setLooping(true);
-      mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-      scaleVideo(mediaPlayer);
-      mediaPlayer.start();
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
-    } catch (SecurityException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  @Override public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
-  }
-
-  @Override public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-    return false;
-  }
-
-  @Override public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-
-  }
-
   @Override public void showLoading() {
 
   }
@@ -141,32 +97,6 @@ public class LogInActivity extends BaseActivity<LogInPresenter>
     LoginManager.getInstance().logOut();
     LoginManager.getInstance()
         .logInWithReadPermissions(this, Arrays.asList("public_profile, email"));
-  }
-
-  private void scaleVideo(MediaPlayer mediaPlayer) {
-    RelativeLayout.LayoutParams videoParams = (RelativeLayout.LayoutParams) trailer
-        .getLayoutParams();
-    DisplayMetrics dm = new DisplayMetrics();
-    getWindowManager().getDefaultDisplay()
-        .getMetrics(dm);
-
-    final int height = dm.heightPixels;
-    final int width = dm.widthPixels;
-    int videoHeight = mediaPlayer.getVideoHeight();
-    int videoWidth = mediaPlayer.getVideoWidth();
-    double hRatio = 1;
-
-    hRatio = (height * 1.0 / videoHeight) / (width * 1.0 / videoWidth);
-    videoParams.width = (int) (hRatio <= 1 ? 0 : Math.round((-(hRatio - 1) / 2)
-        * width));
-    videoParams.height = (int) (hRatio >= 1 ? 0 : Math
-        .round((((-1 / hRatio) + 1) / 2) * height));
-    videoParams.width = width - videoParams.width - videoParams.width;
-    videoParams.height = height - videoParams.height - videoParams.height;
-    trailer.setScaleX(
-        1.00001f);
-    trailer.requestLayout();
-    trailer.invalidate();
   }
 
   private void registerFacebook() {
@@ -215,6 +145,6 @@ public class LogInActivity extends BaseActivity<LogInPresenter>
   }
 
   private void showSnakeBar(String msg) {
-    Snackbar.make(trailer, msg, Snackbar.LENGTH_LONG).show();
+    Snackbar.make(btnFacebook, msg, Snackbar.LENGTH_LONG).show();
   }
 }
